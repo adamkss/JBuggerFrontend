64861b04b1c5a4db0ae198f3db6917e64809e3ed
Kiss ADAM <adam.kiss@msg.group>
2019-04-03 17:18:52 +0300

    Start working on user table

--- src/ProjectSettings.js
+++ src/ProjectSettings.js
@@ -2,16 +2,28 @@ import React, { Component } from 'react'
 import './ProjectSettings.css';
 
 import { connect } from 'react-redux';
-import { Typography, Button } from '@material-ui/core';
+import { Paper, Typography, Button, Table, TableHead, TableRow, TableCell, TableBody, Checkbox } from '@material-ui/core';
 
 import ProjectSettingsSection from './ProjectSettingsSection';
 import LabelShort from './LabelShort';
 import NewLabelDialog from './popovers/NewLabelDialog';
 import { createNewLabel, startCreatingNewLabel, labelCreationAbandoned } from './redux-stuff/actions/actionCreators';
-
+import axios from 'axios';
 class ProjectSettings extends Component {
     state = {
-        isNewLabelDialogOpen: false
+        isNewLabelDialogOpen: false,
+        allUsers: []
+    }
+
+    componentDidMount = () => {
+        if (this.props.isUserPM) {
+            axios.get("http://localhost:8080/users/adminInfo")
+                .then(({ data }) => {
+                    this.setState({
+                        allUsers: data
+                    })
+                })
+        }
     }
 
     openNewLabelDialog = () => {
@@ -25,7 +37,7 @@ class ProjectSettings extends Component {
             isNewLabelDialogOpen: false
         })
     }
-    
+
     onNewLabelDialogCancel = () => {
         this.closeNewLabelDialog();
         this.props.dispatch(labelCreationAbandoned())
@@ -38,7 +50,7 @@ class ProjectSettings extends Component {
     newValueThenWrongWasInserted = () => {
         this.props.dispatch(labelCreationAbandoned());
     }
-    
+
     render() {
         return (
             <>
@@ -51,8 +63,52 @@ class ProjectSettings extends Component {
                         </div>
                     </ProjectSettingsSection>
 
-                    <ProjectSettingsSection sectionName="Users">
-                    </ProjectSettingsSection>
+                    {this.props.isUserPM ?
+                        <ProjectSettingsSection sectionName="Users">
+                            <Paper style={{
+                                maxHeight: "600px",
+                                overflow: "auto",
+                                maxWidth: "1250px",
+                                marginTop: "10px"
+                            }}>
+
+                                <Table style={{
+                                    width: "100%"
+                                }}>
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell>ID</TableCell>
+                                            <TableCell>Name</TableCell>
+                                            <TableCell>Phone number</TableCell>
+                                            <TableCell>Email</TableCell>
+                                            <TableCell>Role</TableCell>
+                                            <TableCell>Username</TableCell>
+                                            <TableCell>Bugs assigned</TableCell>
+                                            <TableCell>Activated</TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {this.state.allUsers.map(user => (
+                                            <TableRow key={user.id}>
+                                                <TableCell>{user.id}</TableCell>
+                                                <TableCell>{user.name}</TableCell>
+                                                <TableCell>{user.phoneNumber}</TableCell>
+                                                <TableCell>{user.email}</TableCell>
+                                                <TableCell>{user.role}</TableCell>
+                                                <TableCell>{user.username}</TableCell>
+                                                <TableCell>{user.bugsAssignedToTheUserIds}</TableCell>
+                                                <TableCell>
+                                                    <Checkbox checked={user.userActivated}></Checkbox>
+                                                </TableCell>
+                                            </TableRow>
+                                        ))}
+                                    </TableBody>
+                                </Table>
+                            </Paper>
+                        </ProjectSettingsSection>
+                        :
+                        null
+                    }
                 </div>
 
                 {this.state.isNewLabelDialogOpen ?
@@ -72,7 +128,8 @@ class ProjectSettings extends Component {
 
 const mapStateToProps = state => ({
     labels: state.bugs.labels,
-    doesNewLabelAlreadyExist: state.bugs.doesNewLabelAlreadyExist
+    doesNewLabelAlreadyExist: state.bugs.doesNewLabelAlreadyExist,
+    isUserPM: state.security.isPM
 });
 
 export default connect(mapStateToProps)(ProjectSettings);
